1cbd57840edd6e645c860a5e0b54ed698d8d1320
EugenioDeve <eugeniodeveloper@outlook.com>
2022-11-14 23:21:48 +0100

    rating js

--- progetto/pagina4/js/script.js
+++ progetto/pagina4/js/script.js
@@ -1,8 +1,34 @@
-var starRatings = document.querySelectorAll('.star-rating');
-for (var index = 0; index < starRatings.length; index++) {
-    var starRating = starRatings[index],
-        fullStars = starRating.querySelector('.full-stars'),
-        rating = parseFloat(starRating.dataset.rating) || 0,
-        percentWidth = rating * 20;
-    fullStars.style.width = percentWidth + '%';
+const ratingStars = [...document.getElementsByClassName("rating_star")];
+const ratingResult = document.querySelector(".rating_result");
+
+
+function executeRating(stars, result) {
+    const starClassActive = "rating_star fas fa-star";
+    const starClassUnactive = "rating_star far fa-star";
+    const starsLength = stars.length;
+    let i;
+    stars.map((star) => {
+        star.onclick = () => {
+            i = stars.indexOf(star);
+
+            if (star.className.indexOf(starClassUnactive) !== -1) {
+                printRatingResult(result, i + 1);
+                for (i; i >= 0; --i) stars[i].className = starClassActive;
+            } else {
+                printRatingResult(result, i);
+                for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
+            }
+        };
+    });
+}
+
+function printRatingResult(result, num = 0) {
+}
+
+
+executeRating(ratingStars, ratingResult); //richiamo funzione
+
+function inviaFeed() {
+    alert('Grazie per aver inviato il tuo Feedback');
+
 }
